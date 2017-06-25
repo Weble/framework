21d595a9421ee326684a6efb057e3061cba3db2a
Miljan Aleksic <miljan@aleksic.es>
2017-06-25 09:54:28 +0200

    remove hard coded admin assets loading

--- libraries/zoolanders/Framework/Listener/Environment/LoadCss.php
+++ libraries/zoolanders/Framework/Listener/Environment/LoadCss.php
@@ -14,9 +14,5 @@ class LoadCss extends Listener {
      * @param \Zoolanders\Framework\Event\Environment\Init $event
      */
     public function handle (\Zoolanders\Framework\Event\Environment\Init $event) {
-        // perform admin tasks
-        if ($event->isAdmin()) {
-            $this->document->addStylesheet('zlfw:assets/css/zl_ui.css');
-        }
     }
 }
