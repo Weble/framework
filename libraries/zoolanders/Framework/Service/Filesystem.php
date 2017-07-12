89e47ec32eab33c22289f2123738b8379e0a32e4
Daniele Rosario <realskullbock@gmail.com>
2017-07-12 11:09:51 +0200

    Reverted old fix for path

--- libraries/zoolanders/Framework/Service/Filesystem.php
+++ libraries/zoolanders/Framework/Service/Filesystem.php
@@ -34,7 +34,7 @@ class Filesystem
     public function __construct (\League\Flysystem\Filesystem $fs = null, Zoo $zoo)
     {
         if (!$fs) {
-            $adapter = new Local(JPATH_ROOT);
+            $adapter = new Local('/');
             $fs = new \League\Flysystem\Filesystem($adapter);
         }
 
